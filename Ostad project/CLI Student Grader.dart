0ba54e2157b1a3b9c425928743d520da10489a5b
NILOY <shilniloy134@gmail.com>
2026-04-10 15:02:50 +0600

    CLI Student Grader - Dart project

--- Ostad project/CLI Student Grader.dart
+++ Ostad project/CLI Student Grader.dart
@@ -1,20 +1,23 @@
 import 'dart:io';
 
 void main(){
-   var name = stdin.readLineSync()!;
+   
    var isrunning = true;
  
+ const String title = "[=== Student Grader v1.0 ===]";
  
- 
-  final Set<String> subject= {
+  final Set<String> availableSubject= {
     "Microprocessor",
     "Data Structure",
     "Object oriented",
     "Internet Programming",
     "Competitive Programming"
   };
+   var students = [];
   do {
-     print("""=== Student Grader v1.0 ==
+    
+    
+     print("""$title
 1. Add Student
 2. Record Score
 3. Add Bonus Points
@@ -26,17 +29,34 @@ void main(){
 
 Choose an option:
   """);
+   
+  
     var choice = stdin.readLineSync()!;
     switch(choice){
       case "1":
+      print("Enter the student name : ");
+    var name = stdin.readLineSync()!;
+      Map <String, dynamic> student = {
+ "name" : name,
+ "Scores" : [],
+ "Subjects" : {...availableSubject},
+ "bonus" : null,
+ "comment" : null
+
+ };
+ students.add(student);
       break;
       case "8":
       isrunning = false;
       break;
+      default:
+      print("Invalid");
     }
   }while(isrunning);
  
-   const String tittle = "[=== Student Grader v1.0 ===]";
-    print(tittle);
+
+ 
+   
+   
   
 }
