df8f40ce4746fccebd97e87982765b7b0b18408c
NILOY <shilniloy134@gmail.com>
2026-04-10 23:17:13 +0600

    Added Record Score and Bonus logic with validation

--- Ostad project/CLI Student Grader.dart
+++ Ostad project/CLI Student Grader.dart
@@ -46,6 +46,43 @@ Choose an option:
  };
  students.add(student);
       break;
+      case "2":
+        // 1. Check kora student ache kina
+        if (students.isEmpty) {
+          print("Age student add korun!");
+        } else {
+          // 2. Numbered list dekhano (Indexed for loop)
+          print("--- Student List ---");
+          for (int i = 0; i < students.length; i++) {
+            print("${i + 1}. ${students[i]['name']}");
+          }
+
+          // 3. Student select kora
+          stdout.write("Student number select korun: ");
+          int index = int.parse(stdin.readLineSync()!) - 1;
+
+          // 4. Subject selection
+          var selectedStudent = students[index];
+          print("Available Subjects: ${selectedStudent['Subjects']}");
+          stdout.write("Subject-er naam likhun: ");
+          String sub = stdin.readLineSync()!;
+
+          // 5. Score Validation (While loop - easy version)
+          double score = -1; 
+          while (score < 0 || score > 100) {
+            stdout.write("$sub er score din (0-100): ");
+            score = double.parse(stdin.readLineSync()!);
+
+            if (score < 0 || score > 100) {
+              print("Vul score! Abar chesta korun.");
+            }
+          }
+
+          // 6. List-e add kora
+          selectedStudent["Scores"].add(score);
+          print("Score successfully add hoyeche!");
+        }
+        break;
       case "8":
       isrunning = false;
       break;
