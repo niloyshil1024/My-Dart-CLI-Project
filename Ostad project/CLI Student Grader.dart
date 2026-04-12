649c8c8cfe9944a6daacae5270221e6bcc76115f
NILOY <shilniloy134@gmail.com>
2026-04-12 15:26:11 +0600

    Add case 3 and case 4

--- Ostad project/CLI Student Grader.dart
+++ Ostad project/CLI Student Grader.dart
@@ -49,7 +49,7 @@ Choose an option:
       case "2":
         // 1. Check kora student ache kina
         if (students.isEmpty) {
-          print("Age student add korun!");
+          print("Add student");
         } else {
           // 2. Numbered list dekhano (Indexed for loop)
           print("--- Student List ---");
@@ -58,13 +58,13 @@ Choose an option:
           }
 
           // 3. Student select kora
-          stdout.write("Student number select korun: ");
+          stdout.write("Select student number ");
           int index = int.parse(stdin.readLineSync()!) - 1;
 
           // 4. Subject selection
           var selectedStudent = students[index];
           print("Available Subjects: ${selectedStudent['Subjects']}");
-          stdout.write("Subject-er naam likhun: ");
+          stdout.write("Enter the subject name: ");
           String sub = stdin.readLineSync()!;
 
           // 5. Score Validation (While loop - easy version)
@@ -74,7 +74,7 @@ Choose an option:
             score = double.parse(stdin.readLineSync()!);
 
             if (score < 0 || score > 100) {
-              print("Vul score! Abar chesta korun.");
+              print("wrong score! pls try again...");
             }
           }
 
@@ -83,6 +83,62 @@ Choose an option:
           print("Score successfully add hoyeche!");
         }
         break;
+        case "3":
+         if (students.isEmpty) {
+          print("Add student");
+        } else {
+          // 2. Numbered list dekhano (Indexed for loop)
+          print("--- Student List ---");
+          for (int i = 0; i < students.length; i++) {
+            print("${i + 1}. ${students[i]['name']}");
+          }
+
+          // 3. Student select kora
+          stdout.write("Select student number ");
+          int index = int.parse(stdin.readLineSync()!) - 1;
+
+        
+        var selectedStudent = students[index];
+        stdout.write("Enter bonus (1-10): ");
+        int bonus = int.parse(stdin.readLineSync()!);
+        if(bonus < 1 || bonus > 10){
+          print("Invalid bonus!");
+
+        }else{
+          if(selectedStudent["bonus"]!= null){
+            print("Bonus already added!");
+          }else{
+            selectedStudent["bonus"] ??= bonus;
+            print("Bonus added successfully!");
+          }
+        }
+        }
+        break;
+        case"4":
+        
+      if (students.isEmpty) {
+       print("Add student first");
+      } else {
+
+      print("--- Student List ---");
+    for (int i = 0; i < students.length; i++) {
+    print("${i + 1}. ${students[i]['name']}");
+    }
+
+     stdout.write("Select student number: ");
+     int index = int.parse(stdin.readLineSync()!) - 1;
+
+    var selectedStudent = students[index];
+
+   stdout.write("Enter comment: ");
+  String comment = stdin.readLineSync()!;
+
+   selectedStudent["comment"] = comment;
+
+   print("Comment added successfully!");
+}
+
+break;
       case "8":
       isrunning = false;
       break;
