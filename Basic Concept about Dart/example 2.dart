cd605ec605206e134f25f53a28d9bb4fd6242155
NILOY <shilniloy134@gmail.com>
2026-04-13 19:43:14 +0600

    feat: add menu loop and switch routing

---
+++ Basic Concept about Dart/example 2.dart
@@ -0,0 +1,11 @@
+class Example2  {
+  int? _age;
+  set setAge(int a){
+    _age = a;
+  }
+
+  get getAge{
+    return _age;
+
+  }
+}
---
+++ Basic Concept about Dart/example.dart
@@ -0,0 +1,10 @@
+class Example {
+  int age = 21;
+  String name = 'learning-dart';
+  myfunction(){
+    print("my function");
+  }
+myfunctionTwo(){
+  print("my functionTwo");
+}
+}
